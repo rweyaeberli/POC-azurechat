de57cff197bad8ea1863066b0e3abec6afa29458
rweyaeberli <r.wey@aeberli.ch>
2024-11-25 21:06:26 +0100

    Update theme-config.ts

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -2,7 +2,7 @@ export const AI_NAME = "Aeberli ChatGPT";
 export const AI_DESCRIPTION = "Wir machen. Jetzt auch mit Unterstützung durch Künstliche Intelligenz.";
 export const AI_DISCLAIMER = "Alle Inhalte, die von ${AI_NAME} bereitgestellt werden, dienen als Unterstützung und Inspiration. Bitte beachte, dass die Künstliche Intelligenz gelegentlich fehlerhafte oder unvollständige Angaben machen kann. Daher ist es unerlässlich, alle Informationen sorgfältig zu prüfen, bevor sie verwendet oder veröffentlicht werden. Aeberli übernimmt keine Haftung für Schäden, die durch ungeprüfte Nutzung der bereitgestellten Inhalte entstehen.";
 
-export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
+export const CHAT_DEFAULT_PERSONA = "Aeberli DNA";
 
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
